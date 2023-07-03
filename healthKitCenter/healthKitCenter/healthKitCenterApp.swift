9a8b134e3dc68982e7a24e1d8542d8fc58c89256
valselee <sollleky72@gmail.com>
2023-07-03 13:00:45 +0900

    [Init] 1. Project Init 2. Dependency를 raw하게 embed

---
+++ healthKitCenter/healthKitCenter/healthKitCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthKitCenterApp.swift
+//  healthKitCenter
+//
+//  Created by Celan on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct healthKitCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
