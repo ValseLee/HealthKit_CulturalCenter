dd05aa21ca5a6ac6d233d890e93733b59ea7cb84
valselee <sollleky72@gmail.com>
2023-07-04 22:13:59 +0900

    [Feat] 1. UIView 기준으로 Wrapping 중

--- healthKitCenter/healthKitCenter/UIComponents/OCKButtonLog.swift
+++ healthKitCenter/healthKitCenter/UIComponents/OCKButtonLog.swift
@@ -6,15 +6,88 @@
 //
 
 import SwiftUI
+import CareKit
+import CareKitUI
+import UIKit
 
-struct OCKButtonLog: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ButtonLogView: UIViewRepresentable {
+    var title: String
+    @Binding var selected: Bool
+    var detail: String?
+    var instruction: String?
+    
+    func makeUIView(context: Context) -> OCKButtonLogTaskView {
+        let buttonLogTaskView = OCKButtonLogTaskView()
+        return buttonLogTaskView
     }
+    
+    func updateUIView(
+        _ uiView: OCKButtonLogTaskView,
+        context: Context
+    ) {
+//        uiView.delegate = context.coordinator
+        
+        uiView.headerView.titleLabel.text = loc(title)
+        
+        uiView.updateItem(at: 0, withTitle: "hi", detail: "detail")
+        
+        if let detail = detail {
+            uiView.headerView.detailLabel.text = loc(detail)
+        }
+        
+        if let instruction {
+//           uiView.instructionsLabel.text = loc(instruction)
+            
+        }
+    }
+    
+    func makeCoordinator() -> Self.Coordinator {
+        Coordinator()
+    }
+    
+//    class Coordinator: NSObject, OCKTaskViewDelegate {
+//        func taskView(
+//            _ taskView: UIView & CareKitUI.OCKTaskDisplayable,
+//            didCompleteEvent isComplete: Bool,
+//            at indexPath: IndexPath,
+//            sender: Any?
+//        ) {
+//            <#code#>
+//        }
+//
+//        func taskView(
+//            _ taskView: UIView & CareKitUI.OCKTaskDisplayable,
+//            didSelectOutcomeValueAt index: Int,
+//            eventIndexPath: IndexPath,
+//            sender: Any?
+//        ) {
+//            <#code#>
+//        }
+//
+//        func taskView(
+//            _ taskView: UIView & CareKitUI.OCKTaskDisplayable,
+//            didCreateOutcomeValueAt index: Int,
+//            eventIndexPath: IndexPath, sender: Any?
+//        ) {
+//            <#code#>
+//        }
+//
+//        func didSelectTaskView(
+//            _ taskView: UIView & CareKitUI.OCKTaskDisplayable,
+//            eventIndexPath: IndexPath
+//        ) {
+//            <#code#>
+//        }
+//    }
 }
 
 struct OCKButtonLog_Previews: PreviewProvider {
     static var previews: some View {
-        OCKButtonLog()
+        ButtonLogView(
+            title: "Title_Mock",
+            selected: .constant(.random()),
+            detail: "Detail_Mock",
+            instruction: "Instruction_Mock"
+        )
     }
 }
--- healthKitCenter/healthKitCenter/UIComponents/OCKInstructionTask.swift
+++ healthKitCenter/healthKitCenter/UIComponents/OCKInstructionTask.swift
@@ -6,15 +6,47 @@
 //
 
 import SwiftUI
+import CareKitUI
 
-struct OCKInstructionTask: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct InstructionTaskView: UIViewRepresentable {
+    var title: String
+    @Binding var selected: Bool
+    var detail: String?
+    var instruction: String?
+    
+    func makeUIView(context: Context) -> OCKInstructionsTaskView {
+        let instructionTaskView = OCKInstructionsTaskView()
+        return instructionTaskView
+    }
+    
+    func updateUIView(
+        _ uiView: OCKInstructionsTaskView,
+        context: Context
+    ) {
+        uiView.headerView.titleLabel.text = loc(title)
+        
+        if let detail = detail {
+            uiView.headerView.detailLabel.text = loc(detail)
+        }
+        
+        if let instruction {
+            uiView.instructionsLabel.text = loc(instruction)
+        }
+        
+        uiView.completionButton.isSelected = selected
     }
 }
 
-struct OCKInstructionTask_Previews: PreviewProvider {
+struct InstructionTaskView_Previews: PreviewProvider {
     static var previews: some View {
-        OCKInstructionTask()
+        VStack {
+//        ScrollView {
+            InstructionTaskView( // View가 사라짐니다..?
+                title: "title_Mock",
+                selected: .constant(.random()),
+                detail: "Detail_Mock",
+                instruction: "이렇게 하세요."
+            )
+        }
     }
 }
