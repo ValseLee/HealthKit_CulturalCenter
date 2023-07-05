3f74dd3092b7438308ca8820f63558e3d133dac7
valselee <sollleky72@gmail.com>
2023-07-05 12:28:52 +0900

    [Fix] 1. ScrollView 내부에서 Size가 깨지는 이슈로 인해 Card가 사라지는 현상 해결 2. SizeThatFits() 로 임시 해결 완료

--- healthKitCenter/healthKitCenter/UIComponents/OCKInstructionTask.swift
+++ healthKitCenter/healthKitCenter/UIComponents/OCKInstructionTask.swift
@@ -25,6 +25,12 @@ struct InstructionTaskView: UIViewRepresentable {
     ) {
         uiView.headerView.titleLabel.text = loc(title)
         
+        // MARK: Do Not Work
+//        uiView.setContentCompressionResistancePriority(
+//            .defaultHigh,
+//            for: .vertical
+//        )
+        
         if let detail = detail {
             uiView.headerView.detailLabel.text = loc(detail)
         }
@@ -35,18 +41,33 @@ struct InstructionTaskView: UIViewRepresentable {
         
         uiView.completionButton.isSelected = selected
     }
+    
+    func sizeThatFits(
+        _ proposal: ProposedViewSize,
+        uiView: OCKInstructionsTaskView,
+        context: Context
+    ) -> CGSize? {
+//        uiView.sizeThatFits(.init(width: 300, height: 300))
+//        uiView.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        
+        return .init(
+            width: UIScreen.main.bounds.width - 48,
+            height: 300
+        )
+    }
 }
 
 struct InstructionTaskView_Previews: PreviewProvider {
     static var previews: some View {
-        VStack {
-//        ScrollView {
+//        VStack {
+        ScrollView {
             InstructionTaskView( // View가 사라짐니다..?
                 title: "title_Mock",
                 selected: .constant(.random()),
                 detail: "Detail_Mock",
                 instruction: "이렇게 하세요."
             )
+//            .padding()
         }
     }
 }
