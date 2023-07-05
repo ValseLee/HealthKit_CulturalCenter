1d71bd7b0934a00624a0d35e268060c7ffee4032
valselee <sollleky72@gmail.com>
2023-07-05 17:18:37 +0900

    [Feat] 1. ContentView에서 Mock 뷰 확인 2. ScrollView 에서 래핑된 UIKitView 잘 보이는 거 확인

--- healthKitCenter/healthKitCenter/ContentView.swift
+++ healthKitCenter/healthKitCenter/ContentView.swift
@@ -5,104 +5,49 @@
 //  Created by Celan on 2023/07/03.
 //
 
-import SwiftUI
-import CareKitUI
 import CareKit
+import CareKitUI
+import SwiftUI
 
 struct ContentView: View {
+    @State private var simpleSelected: Bool = false
+    @State private var instructionSelected: Bool = false
+    
     var body: some View {
-        CardView {
-            Text("?")
-        }
-    }
-}
-
-public struct CircularCompletionView<Content: View>: View {
-
-    @Environment(\.careKitStyle) private var style
-
-    private var fillColor: Color {
-        isComplete ? Color.accentColor : Color(style.color.clear)
-    }
-
-    private var background: some View {
-        GeometryReader { geo in
-            ZStack {
-                Circle().fill(self.fillColor)
-                Circle().strokeBorder(
-                    Color.accentColor,
-                    lineWidth: 5
-                )
-            }
-            .inverseMask(self.content)
-        }
-    }
-
-    private let content: Content
-    private let isComplete: Bool
-
-    public var body: some View {
-        VStack {
-            // The content helps determine the frame, but it does not need to be visible because it will be cut into the circle using an
-            // `inverseMask`.
-            content.hidden()
-        }
-        .clipShape(Circle())
-        .background(background)
-        .font(Font.body.weight(.bold))
-    }
-
-    /// Create an instance.
-    /// - Parameters:
-    ///   - isComplete: The completion state that affects the style of the view.
-    ///   - content: The content of the view. The content will be vertically stacked.
-    public init(isComplete: Bool, @ViewBuilder content: () -> Content) {
-        self.isComplete = isComplete
-        self.content = content()
-    }
-}
-
-#if DEBUG
-struct CircularCompletionViewPreviews: PreviewProvider {
-    static var previews: some View {
         ScrollView {
-            VStack(spacing: 20) {
-
-                CircularCompletionView(isComplete: true) {
-                    Text("Complete")
-                        .padding()
-                        .frame(height: 90)
-                }
-
-                CircularCompletionView(isComplete: false) {
-                    Image(systemName: "checkmark")
-                        .resizable()
-                        .padding()
-                        .frame(width: 50, height: 50)
-                }
-
-                CircularCompletionView(isComplete: true) {
-                    Image(systemName: "checkmark")
-                        .resizable()
-                        .padding()
-                        .frame(width: 40, height: 40)
-                }
-
-                CircularCompletionView(isComplete: false) {
-                    Image(systemName: "checkmark")
-                        .resizable()
-                        .padding()
-                        .frame(width: 30, height: 30)
-                }
-
-                CircularCompletionView(isComplete: false) {
-                    Image(systemName: "checkmark")
-                        .resizable()
-                        .padding()
-                        .frame(width: 20, height: 20)
-                }
-            }
+            SimpleTaskView(
+                title: "Title",
+                selected: $simpleSelected,
+                detail: "Details"
+            )
+            .frame(
+                maxWidth: .infinity,
+                minHeight: 100,
+                maxHeight: 200
+            )
+            .padding()
+            
+            InstructionTaskView(
+                title: "Title",
+                selected: $instructionSelected,
+                detail: "Detail",
+                instruction: "Instruction: Do this Do That"
+            )
+            .frame(
+                maxWidth: .infinity,
+                minHeight: 100,
+                maxHeight: 300
+            )
+            .padding()
+            
         }
     }
 }
-#endif
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
