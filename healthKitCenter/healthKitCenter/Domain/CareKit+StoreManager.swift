2001074175a440521c59f495acccc40440786ac5
valselee <sollleky72@gmail.com>
2023-07-08 21:41:02 +0900

    [Add] 1. OCKStore, CareKitStoreContainer 파일 생성

---
+++ healthKitCenter/healthKitCenter/Domain/CareKit+StoreManager.swift
@@ -0,0 +1,8 @@
+//
+//  CareKit+StoreManager.swift
+//  healthKitCenter
+//
+//  Created by Celan on 2023/07/08.
+//
+
+import Foundation
---
+++ healthKitCenter/healthKitCenter/Domain/CareKitStore+Tasks.swift
@@ -0,0 +1,15 @@
+//
+//  AppStore.swift
+//  healthKitCenter
+//
+//  Created by Celan on 2023/07/08.
+//
+
+import SwiftUI
+import CareKitStore
+
+extension CareKitStore {
+    enum Tasks: String, CaseIterable {
+        case sleepRoutine
+    }
+}
