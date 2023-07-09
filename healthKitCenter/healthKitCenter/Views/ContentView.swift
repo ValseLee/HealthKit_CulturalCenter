7089bb064d111d1beffd1e3c5ce9c8e73c9b9aca
valselee <sollleky72@gmail.com>
2023-07-09 20:30:37 +0900

    [Feat] 1. 앱의 시작과 함께 어떤 Care Routine을 원하는지 선택하도록 구현 2. 해당 유저의 선택에 따라 OCKTask가 지정되고 OCKStore Init

--- healthKitCenter/healthKitCenter/Views/ContentView.swift
+++ healthKitCenter/healthKitCenter/Views/ContentView.swift
@@ -5,75 +5,24 @@
 //  Created by Celan on 2023/07/03.
 //
 
-import SwiftUI
 import CareKit
 import CareKitStore
+import SwiftUI
 
 struct ContentView: View {
     @State private var tabs = "first"
-    @State private var isSleepCourseSelected: Bool = false
-    @State private var isEatingCourseSelected: Bool = false
     
     var body: some View {
         TabView(selection: $tabs) {
-            firstStep
+            CareRoutineSelectView(currentTab: $tabs)
                 .tag("first")
             
-            secondStep
-                .tag("second")
+            SleepRoutineSettingView(currentTab: $tabs)
+                .tag(OCKStore.Tasks.sleep.rawValue)
         }
         .tabViewStyle(.page)
     }
     
-    var firstStep: some View {
-        VStack {
-            Text("Which Care Course do you want?")
-                .bold()
-                .padding(.bottom, 32)
-            
-            Button {
-                withAnimation {
-                    if isEatingCourseSelected { isEatingCourseSelected = false }
-                    if isSleepCourseSelected { isSleepCourseSelected = false }
-                    isSleepCourseSelected = true
-                }
-            } label: {
-                Label(
-                    "Sleep",
-                    systemImage: isSleepCourseSelected ? "square.fill" : "square"
-                )
-                .frame(width: 100, height: 30)
-            }
-            
-            Button {
-                withAnimation {
-                    if isSleepCourseSelected { isSleepCourseSelected = false }
-                    if isEatingCourseSelected { isEatingCourseSelected = false }
-                    isEatingCourseSelected = true
-                }
-            } label: {
-                Label(
-                    "Diet",
-                    systemImage: isEatingCourseSelected ? "square.fill" : "square"
-                )
-                .frame(width: 100, height: 30)
-            }
-            
-            if isEatingCourseSelected || isSleepCourseSelected {
-                Button {
-                    withAnimation {
-                        tabs = "second"
-                    }
-                } label: {
-                    Text("next")
-                }
-            }
-        }
-    }
-    
-    var secondStep: some View {
-        Text("HO")
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- healthKitCenter/healthKitCenter/Views/SleepRoutineSettingView.swift
+++ healthKitCenter/healthKitCenter/Views/SleepRoutineSettingView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct SleepRoutineSettingView: View {
+    @EnvironmentObject var storeManager: CareKitStoreContainer
+    @Binding var currentTab: String
+    @State private var date: Date = .now
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        DatePicker(
+            "",
+            selection: $date,
+            displayedComponents: .hourAndMinute
+        )
+            .labelsHidden()
+            .datePickerStyle(.wheel)
     }
 }
 
 struct SleepRoutineSettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SleepRoutineSettingView()
+        SleepRoutineSettingView(currentTab: .constant(""))
     }
 }
