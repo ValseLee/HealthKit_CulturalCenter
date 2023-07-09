847e7fe4ef06cfb110de75144934d1b255bb6a10
valselee <sollleky72@gmail.com>
2023-07-09 20:29:46 +0900

    [Feat] OCKTask 를 추가하는 메소드를 내부에 확장 구현

--- healthKitCenter/healthKitCenter/Domain/Extensions/OCKStore+Tasks.swift
+++ healthKitCenter/healthKitCenter/Domain/Extensions/OCKStore+Tasks.swift
@@ -10,6 +10,36 @@ import CareKitStore
 
 extension OCKStore {
     enum Tasks: String, CaseIterable {
-        case sleepRoutine
+        case sleep
+        case diet
+    }
+    
+    func _initTasks() {
+        var tasks: [OCKTask] = []
+        tasks.append(self.makeTasks(on: 1))
+        
+        addTasks(
+            tasks,
+            callbackQueue: .main
+        ) { result in
+            switch result {
+            case .success: break
+            case let .failure(err): dump(err)
+            }
+        }
+    }
+    
+    // MARK: Methods
+    func makeTasks(on days: Int) -> OCKTask {
+        let task = OCKTask(
+            id: OCKStore.Tasks.sleep.rawValue,
+            title: OCKStore.Tasks.sleep.rawValue,
+            carePlanUUID: nil,
+            // 11:00
+            schedule: .dailyAtTime(hour: 23, minutes: 0, start: .now.adding(days: -days), end: nil, text: nil)
+        )
+        print(#function, task)
+        
+        return task
     }
 }
