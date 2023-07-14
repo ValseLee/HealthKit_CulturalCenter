cb2491405475ba62137d2b098a58de03b669136c
valselee <sollleky72@gmail.com>
2023-07-14 13:19:40 +0900

    [Add] SleepRoutineTaskView 파일 생성

---
+++ healthKitCenter/healthKitCenter/Views/SleepRoutineTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  SleepRoutineTaskView.swift
+//  healthKitCenter
+//
+//  Created by Celan on 2023/07/14.
+//
+
+import SwiftUI
+
+struct SleepRoutineTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SleepRoutineTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        SleepRoutineTaskView()
+    }
+}
