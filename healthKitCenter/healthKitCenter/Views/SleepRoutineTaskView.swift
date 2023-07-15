4d5c5b919f69d88092a979953d1af419582a0697
valselee <sollleky72@gmail.com>
2023-07-15 21:54:35 +0900

    [Feat] 1. CareKitUIContainer의 body를 래핑하여 ButtonLogTaskView 를 생성 2. SwiftUI 내에서의 뷰 레이아웃 작업 필요

--- healthKitCenter/healthKitCenter/Views/SleepRoutineTaskView.swift
+++ healthKitCenter/healthKitCenter/Views/SleepRoutineTaskView.swift
@@ -5,12 +5,40 @@
 //  Created by Celan on 2023/07/14.
 //
 
+import CareKit
+import CareKitStore
 import SwiftUI
 
 struct SleepRoutineTaskView: View {
+    @EnvironmentObject var myCareStore: OCKStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SleepRoutineButtonTaskView(myCareStore: myCareStore)
     }
+}
+
+struct SleepRoutineButtonTaskView: UIViewControllerRepresentable {
+    let myCareStore: OCKStore
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let container = CareKitUIContainer(style: .buttonLog(myCareStore))
+        return container.body
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+//    func sizeThatFits(
+//        _ proposal: ProposedViewSize,
+//        uiViewController: some UIViewControllerType,
+//        context: Context
+//    ) -> CGSize? {
+//        return CGSize(
+//            width: UIScreen.main.bounds.width - 20,
+//            height: 400
+//        )
+//    }
 }
 
 struct SleepRoutineTaskView_Previews: PreviewProvider {
