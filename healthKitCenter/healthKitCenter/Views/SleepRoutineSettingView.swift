f74a4564e1bfcdc6b1aeeb8df1a4abbc367bae63
valselee <sollleky72@gmail.com>
2023-07-15 21:52:27 +0900

    [Feat] OCKStore에 단일 Task 추가하고 ButtonLog에서 관리할 수 있도록 VC 연결

--- healthKitCenter/healthKitCenter/Views/SleepRoutineSettingView.swift
+++ healthKitCenter/healthKitCenter/Views/SleepRoutineSettingView.swift
@@ -6,20 +6,81 @@
 //
 
 import SwiftUI
+import CareKitStore
 
 struct SleepRoutineSettingView: View {
-    @EnvironmentObject var storeManager: CareKitStoreContainer
+    @EnvironmentObject var myCareStore: OCKStore
     @Binding var currentTab: String
     @State private var date: Date = .now
+    @State private var isSleepCareRoutineConfirmed: Bool = false
     
     var body: some View {
-        DatePicker(
-            "",
-            selection: $date,
-            displayedComponents: .hourAndMinute
-        )
-            .labelsHidden()
-            .datePickerStyle(.wheel)
+        VStack {
+            Text("What time do you want to get to sleep?")
+            
+            DatePicker(
+                "",
+                selection: $date,
+                displayedComponents: .hourAndMinute
+            )
+                .labelsHidden()
+                .datePickerStyle(.wheel)
+            
+            Text(date.formatted())
+            
+            Button {
+                Task {
+                    do {
+                        // Init Store's Care Routine
+//                        let _ = await myCareStore.makeTasks(
+//                            on: date,
+//                            "Please Tap the button, when you're about to sleep :)"
+//                        )
+                        
+                        do {
+                            let _ = try await myCareStore.addTask(
+                                OCKTask(
+                                    id: OCKStore.Tasks.sleep.rawValue,
+                                    title: OCKStore.Tasks.sleep.rawValue,
+                                    carePlanUUID: nil,
+                                    schedule: .dailyAtTime(
+                                        hour: Date.getHour(from: date),
+                                        minutes: Date.getHour(from: date),
+                                        start: date,
+                                        end: nil,
+                                        text: "Please Tap the button, when you're about to sleep :)"
+                                    )
+                                )
+                            )
+                        } catch {
+                            dump(error)
+                        }
+                        
+                        withAnimation {
+                            isSleepCareRoutineConfirmed = true
+                        }
+                    }
+                }
+                
+            } label: {
+                VStack {
+                    Text("Confirm")
+                    Text(
+                    """
+                    \(Date.getHour(from: date).formatted()) : \(Date.getMinute(from: date).formatted())
+                    """
+                    )
+                }
+            }
+            
+            if isSleepCareRoutineConfirmed {
+                Button {
+                    currentTab = "\(OCKStore.Tasks.sleep.rawValue)Task"
+                } label: {
+                    Text("GO To My Routine")
+                }
+            }
+        }
     }
 }
 
