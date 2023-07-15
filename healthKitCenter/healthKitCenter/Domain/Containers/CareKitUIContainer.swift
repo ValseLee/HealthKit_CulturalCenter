f0c77a54410c67a70f65973ae8380297f9e5fa72
valselee <sollleky72@gmail.com>
2023-07-16 00:30:56 +0900

    [Feat] 1. CareKitUIContainer 구현 2. style에 따라 ButtonLog, Simple, InstructionTask VC를 리턴하는 body 구 3. 각 OCK 관련 VC는 열거형 연관값의 store를 기반으로 뷰를 구성함

--- healthKitCenter/healthKitCenter/Domain/Containers/CareKitUIContainer.swift
+++ healthKitCenter/healthKitCenter/Domain/Containers/CareKitUIContainer.swift
@@ -5,4 +5,45 @@
 //  Created by Celan on 2023/07/09.
 //
 
-import Foundation
+import CareKit
+import CareKitStore
+import UIKit
+
+struct CareKitUIContainer {
+    enum CareKitUIStyles {
+        case buttonLog(OCKAnyStoreProtocol)
+        case simpleTask(OCKAnyStoreProtocol)
+        case instructionTask(OCKAnyStoreProtocol)
+    }
+    
+    public var style: CareKitUIStyles
+    
+    public var body: UIViewController {
+        switch style {
+        case let .buttonLog(store):
+            let buttonLogTaskVC = OCKButtonLogTaskViewController(
+                query: OCKEventQuery(for: .now),
+                // App에서 관리하는 OCKStore를 전달하여 VC 구성
+                store: store
+            )
+            
+            return buttonLogTaskVC
+            
+        case let .simpleTask(store):
+            let simpleTaskVC = OCKSimpleTaskViewController(
+                query: .init(for: .now),
+                store: store
+            )
+            
+            return simpleTaskVC
+            
+        case let .instructionTask(store):
+            let instructionTaskVC = OCKInstructionsTaskViewController(
+                query: .init(for: .now),
+                store: store
+            )
+            
+            return instructionTaskVC
+        }
+    }
+}
