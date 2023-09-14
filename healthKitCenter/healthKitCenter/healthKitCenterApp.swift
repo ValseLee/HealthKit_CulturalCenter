1771c8f6f6f1ba724d331eb4790c37b95b577e0d
valselee <sollleky72@gmail.com>
2023-09-14 23:05:32 +0900

    [Chore] CareKit Store 전달

--- healthKitCenter/healthKitCenter/healthKitCenterApp.swift
+++ healthKitCenter/healthKitCenter/healthKitCenterApp.swift
@@ -5,13 +5,18 @@
 //  Created by Celan on 2023/07/03.
 //
 
+import CareKitStore
 import SwiftUI
 
 @main
 struct healthKitCenterApp: App {
+    @StateObject var myCareStore = CareKitStoreContainer()
+        .myCareStore
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(myCareStore)
         }
     }
 }
